f77de85da1de7c7a0f84c9dc3f5862d97f0c1ee6
AndrewKushnir <cuckop30@gmail.com>
2024-01-31 22:11:35 +0300

    Update Program.cs

--- password_gen_project_backend/Program.cs
+++ password_gen_project_backend/Program.cs
@@ -6,6 +6,8 @@ builder.Services.AddGrpc();
 
 var app = builder.Build();
 
+//its rework
+
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
